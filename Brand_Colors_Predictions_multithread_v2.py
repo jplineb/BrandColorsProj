0307815678026ae59027645f749158e6eb2e554e
John Lineberger <38058488+jplineb@users.noreply.github.com>
2019-07-08 10:05:40 -0400

    Add files via upload
    Modified the class script and added argument parsing

--- Brand_Colors_Predictions_multithread_v2.py
+++ Brand_Colors_Predictions_multithread_v2.py
@@ -11,60 +11,55 @@ from imutils.video import FileVideoStream
 import argparse
 import imutils
 
-from multithreadimageload import InferenceDataStream
+from multithreadimageload import InferenceDataStream, ColorCorrector
 
 import psutil
 
-def normalize(img):
-    norm_img=img.clone().detach()
-    norm_img[0]=(norm_img[0]-.485)/.229
-    norm_img[1]=(norm_img[1]-.456)/.224
-    norm_img[2]=(norm_img[2]-.406)/.225
-  
-    return norm_img
+##
+## Read off arguments
+
+# initiate the parser
+parser = argparse.ArgumentParser()
+
+# add long and short argument
+parser.add_argument("--profile", "-p", help="Run line profiling", action='store_true')
+parser.add_argument("--intensity", "-i", help="Scale factor for the correction mask", default=0.4)
+
+# read arguments from the command line
+args = parser.parse_args()
 
 
+##
+## Load the pytorch model and set video clip
+model= torch.load('../Models/ColorResNet_0_1_3.pt').eval()
+#video='FB-112418-USCClemson-Clip3_360p.mp4'
+video = 'clip2'
 
-def predict_one(rgb_image, frame_number):
-    #option 2
-    #rgb_image = torch.tensor(img,dtype=torch.float16,device=torch.device('cuda:0'))
-    #alpha = multithreadimageload(img, queue_size = 25).start() # starts multithreadreadimageload
-    #rgb_image = alpha.read() # grabs the next frame from the queue
-    #rgb_image = rgb_image.permute(2,0,1) #moved to multithread code
-    #rgb_image = rgb_image/255 # moved to multithread code
-    if frame_number%1 == 0 or frame_number <= 5:
-    	normed=normalize(rgb_image)
-    	normed=normed[None]
-    	global prediction_orig
-    	prediction_orig=model(normed)
-    	prediction =(rgb_image + .4*(2*prediction_orig-1)).clamp(0,1)
-    else:
-    	prediction =(rgb_image + .4*(2*prediction_orig-1)).clamp(0,1)
-    #alpha.stop()
-    return prediction
 
-# read in a video file and display corrected version "on the fly"
-def livevideocorrection(filepath, modelname):
-	fvs = FileVideoStream(filepath, queue_size=25).start()
-	ivs = InferenceDataStream(fvs, queue_size=25).start() # --> inference video stream reads from file video stream and forms queue of prepped images for inference
-	desired_frames = 1000
+##
+## create the video and inference streams 
+fvs = FileVideoStream(video, queue_size=15).start()
+ivs = InferenceDataStream(fvs, model, queue_size=15).start()
+
+##
+## set up the color corrector
+cc = ColorCorrector(model, intensity=float(args.intensity))
+
+##
+## The function for looping over the video
+def livevideocorrection():
+
+	desired_frames = 2000
 	frame_number = 0
 	last_time = time.monotonic()
-	cv2.namedWindow('preview', cv2.WINDOW_NORMAL) # creates cv2 window entity called 'preview'
-	cv2.resizeWindow('preview', (1920, 1080)) # resizes cv2 window entitiy called 'preview'
+	#cv2.namedWindow('preview', cv2.WINDOW_NORMAL) # creates cv2 window entity called 'preview'
+	#cv2.resizeWindow('preview', (1920, 1080)) # resizes cv2 window entitiy called 'preview'
 	while ivs.more() and frame_number <= desired_frames:
-		#frame = fvs.read() # --> frame = ivs.read()
-		frame = ivs.read()
+		
+		# get corrected image
+		prediction = cc.predict(ivs.read(), frame_number)
 
-		if frame_number%10 == 0:
-			current_time = time.monotonic()
-			timedif = current_time - last_time
-			FPS = 10/timedif
-			print('FPS:' + str(FPS))
-			last_time = time.monotonic()
-		#frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-		prediction = predict_one(frame, frame_number)
-		torch.cuda.synchronize()
+		# postprocess the image for display
 		prediction = prediction.float()
 		prediction = prediction.squeeze()
 		prediction = prediction.detach()
@@ -72,33 +67,44 @@ def livevideocorrection(filepath, modelname):
 		prediction = prediction.cpu()
 		prediction = prediction.numpy()
 		prediction = cv2.cvtColor(prediction, cv2.COLOR_BGR2RGB)
-		
+
+		# print fps every ten frames
+		if frame_number%10 == 0:
+			current_time = time.monotonic()
+			timedif = current_time - last_time
+			FPS = 10/timedif
+			print('FPS:' + str(FPS))
+			last_time = time.monotonic()
+		frame_number += 1
+
+		# update the display
 		cv2.putText(prediction, "Queue Size (FVS/IVS): {}/{}".format(fvs.Q.qsize(), ivs.Q.qsize()),
 			(10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
 		cv2.putText(prediction, ('FPS: ' + str(FPS)),(750, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2) 
 		cv2.imshow('preview', prediction)
-		frame_number += 1
 		cv2.waitKey(1)
 		
+
 		
-	
+	# clean up the environment
 	cv2.destroyAllWindows()
+	ivs.stop()
 	fvs.stop()
 
-model= torch.load('../Models/ColorResNet_0_1_3.pt')
-model.eval()
 
-video='FB-112418-USCClemson-Clip3_360p.mp4'
-#livevideocorrection(video, model)
+if __name__=='__main__':
+	
+	if args.profile:
+		lp = LineProfiler()
+		lp.add_function(ColorCorrector.predict)
+		lp_wrapper = lp(livevideocorrection)
+		lp_wrapper()
+		lp.print_stats()
+	else:
+		livevideocorrection()
+	
 
-
-lp = LineProfiler()
-lp.add_function(predict_one)
-lp_wrapper = lp(livevideocorrection)
-lp_wrapper(video, model)
-lp.print_stats()
-
-print('memory % used: '+ str(psutil.virtual_memory()[2]))
+	print('memory % used: '+ str(psutil.virtual_memory()[2]))
 
 
 
--- multithreadimageload.py
+++ multithreadimageload.py
@@ -1,10 +1,108 @@
 from threading import Thread
 import sys
 import torch
+import torchvision.transforms as transforms
 import time
 import cv2
 from queue import Queue
 
+class ColorCorrector:
+	
+	# class members
+	norm_mu = torch.tensor([0.485, 0.456, 0.406], dtype=torch.float16,device=torch.device('cuda:0')).reshape(3,1,1)
+	norm_sd = torch.tensor([0.229, 0.224, 0.225], dtype=torch.float16,device=torch.device('cuda:0')).reshape(3,1,1)
+	
+	def __init__(self, model, intensity=0.4):
+		self.model=model
+		self.intensity=intensity
+	 
+	def predict(self, rgb_image, frame_number):
+
+		if frame_number%1 == 0 or frame_number <= 5:
+			img_normed = ColorCorrector._normalize(rgb_image)[None]
+			self.prediction_orig = self.model(img_normed)
+
+		prediction =rgb_image + self.intensity*2.0*self.prediction_orig-self.intensity
+
+		return prediction.clamp(0,1)
+		
+	@classmethod
+	def _normalize(cls, img):
+		norm_img=img.clone().detach()
+		return (norm_img - cls.norm_mu)/cls.norm_sd
+
+class InferenceDataStream:
+	
+	def __init__(self, fvs, model, transforms=None, queue_size=25):
+		
+		# initialize the process
+		self.stream = fvs
+		self.model = model
+		self.stopped = False
+		self.transfrom = transforms
+		
+		# Initialize the Queue
+		self.Q = Queue(maxsize=queue_size)
+		# initialize thread
+		self.thread = Thread(target=self.update, args=())
+		self.thread.daemon = True
+
+		self.tries=0
+		
+	def start(self):
+		# start a thread to load tensors onto gpu from frames
+		self.thread.start()
+		return self
+		
+	def update(self):
+		#keep looping infinitely
+		while True:
+			# keep track of the number of frames
+			
+			if self.stopped:
+				break
+			
+			# if Q is not full read another into it
+			if not self.Q.full():
+				# read from file stream
+				img = self.stream.read()
+				# change colors
+				img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+				# format tensor type
+				frame = torch.tensor(img,dtype=torch.float16,device=torch.device('cuda:0')).permute(2,0,1)/255
+
+				self.Q.put(frame) # adds frame to the queue
+				
+			else:
+				time.sleep(0.2) #rest for 10ms, we have a full queue
+		
+	def read(self):
+		# returns next frame to load in the queue
+		return self.Q.get()
+		
+	def running(self):
+		return self.more() or not self.stopped
+		
+	def more(self):
+		#returns True if there are still frames to be loaded
+		while self.Q.qsize() == 0 and not self.stopped and self.tries < 5:
+			time.sleep(0.1)
+			self.tries += 1
+
+		self.tries=0
+	
+		return self.Q.qsize() > 0 
+		
+	def stop(self):
+		# inidcate that the thread should be stopped
+		self.stopped = True
+		# wait until stream resource are released
+		#self.thread.join()
+		
+
+		
+
+"""
 class multithreadimageload:
 	def __init__(self, img, transforms=None, queue_size=25):
 		# initialize the process
@@ -59,67 +157,8 @@ class multithreadimageload:
 		self.stopped = True
 		# wait until stream resource are released
 		#self.thread.join()
+"""
 
-class InferenceDataStream:
-	def __init__(self, fvs, transforms=None, queue_size=25):
-		# initialize the process
-		self.stream = fvs
-		
-		self.stopped = False
-		self.transfrom = transforms
-		
-		# Initialize the Queue
-		self.Q = Queue(maxsize=queue_size)
-		# initialize thread
-		self.thread = Thread(target=self.update, args=())
-		self.thread.daemon = True
-
-		self.tries=0
-		
-	def start(self):
-		# start a thread to load tensors onto gpu from frames
-		self.thread.start()
-		return self
-		
-	def update(self):
-		#keep looping infinitely
-		while True:
-			if self.stopped:
-				break
-			
-			if not self.Q.full():
-				#Load the next image from
-				img = self.stream.read()
-				img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-				frame = torch.tensor(img,dtype=torch.float16,device=torch.device('cuda:0')).permute(2,0,1)/255
-				#run predict
-				self.Q.put(frame) # adds frame to the queue
-				
-			else:
-				time.sleep(0.2) #rest for 10ms, we have a full queue
-		
-	def read(self):
-		# returns next frame to load in the queue
-		return self.Q.get()
-		
-	def running(self):
-		return self.more() or not self.stopped
-		
-	def more(self):
-		#returns True if there are still frames to be loaded
-		while self.Q.qsize() == 0 and not self.stopped and self.tries < 5:
-			time.sleep(0.1)
-			self.tries += 1
-
-		self.tries=0
-	
-		return self.Q.qsize() > 0 
-		
-	def stop(self):
-		# inidcate that the thread should be stopped
-		self.stopped = True
-		# wait until stream resource are released
-		#self.thread.join()
 
 				
 	
